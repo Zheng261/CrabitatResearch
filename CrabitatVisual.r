472fbf9b62acedd0aa0de38c40e9c7ec5b8f6126
Unknown <yzh@stanford.edu>
2018-07-13 14:54:38 -0700

    Decomposition and over-time track plotting
    Separated code into more files and added plots to identify tracks over time

--- CrabitatVisual.r
+++ CrabitatVisual.r
@@ -16,20 +16,43 @@ plot(classed,add=TRUE,alpha=0.5)
 
 shapefiledir = "../Palmyra Crab Research/Crab tagging/crab tracks 2017/speed-filtered shapefiles/crab-01-speedtrimmed.shp"
 shapefile = st_read(shapefiledir)
-shapefile2 <- st_transform(shapefile, "+proj=utm +zone=3 +ellps=WGS84 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs")
-plot(shapefile2, pch=15,cex=0.3,add=TRUE)
+shapefile2 <- st_transform(shapefile, "+proj=utm +zone=3 +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0")
+#plot(shapefile2, pch=15,cex=0.3,add=TRUE)
 
 crabList = list()
 for (crab in unique(crabs201X$CrabNum)) {
   crabList[[crab]] = Lines(list(Line(crabs201X[which(crabs201X$CrabNum == crab),c("Longitude","Latitude")])),ID=crab)
 }
-
+#Converts coordinates to a spatial lines object
 testSL = SpatialLines((crabList[101]))
-
 testSL@lines = crabList[unique(crabs201X$CrabNum)]
 proj4string(testSL) <- CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")
-testUTMSL = spTransform(testSL,"+proj=utm +zone=3 +ellps=WGS84 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs")
+testUTMSL = spTransform(testSL,"+proj=utm +zone=3 +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0")
+testUTMSL@lines
+plot(testUTMSL,pch=15,cex=0.3,lend=15,ljoin=4,lwd=2,lty=1,add=TRUE)
+dev.off()
 
-plot(testUTMSL,col=c("green","red","blue","yellow","purple","pink"),pch=15,cex=0.3,lend=15,ljoin=4,lwd=2,lty=1)
+
+#Plots individual crab tracks per island, by day
+pdf("CrabVisualTracks.pdf")
+for (crab in unique(crabs201X$CrabNum)) {
+  crabList = list()
+  thisCrabTrax = crabs201X[which(crabs201X$CrabNum == crab),]
+  island = thisCrabTrax[1,"Island"]
+  imgtest <- brick(paste0(island,".tif"))
+  #proj4string(imgtest)
+  plotRGB(imgtest, r=1, g=2, b=3,stretch="lin")
+  for (date in 1:length(unique(thisCrabTrax$Date))) {
+    crabList[[date]] = Lines(list(Line(thisCrabTrax[which(thisCrabTrax$Date == unique(thisCrabTrax$Date)[date]),c("Longitude","Latitude")])),ID=date)
+  }
+  colors = rainbow(length(unique(thisCrabTrax$Date)))
+  testSL = SpatialLines((crabList[1]))
+  testSL@lines <- crabList
+  proj4string(testSL) <- CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")
+  testUTMSL = spTransform(testSL,"+proj=utm +zone=3 +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0")
+  #plot(testUTMSL,col=colors,pch=15,cex=0.3,lend=15,ljoin=4,lwd=2,lty=1)
+  plot(testUTMSL,col=colors,pch=15,cex=0.3,lend=15,ljoin=4,lwd=2,lty=1,add=TRUE)
+  legend(title=paste("Crab:",crab),x=(imgtest@extent[1]+imgtest@extent[2])/2,y=(imgtest@extent[3]+imgtest@extent[4])/2,legend=unique(thisCrabTrax$Date),col=colors,lty=1,cex=0.75)
+}
 dev.off()
 
--- crabs1617crossover.r
+++ crabs1617crossover.r
@@ -1,5 +1,6 @@
 # Import smoothed classification image:
-classed <- raster("6.26MASKEDPalmyra-RF-classification-v3-5x5-modal.tif") # Took 5x5 MODAL average
+#classed <- raster("6.26MASKEDPalmyra-RF-classification-v3-5x5-modal.tif") # Took 5x5 MODAL average
+#classed <- raster("Palmyra-RF-classification-v3-5x5-modal.tif") # Took 5x5 MODAL average
 
 ####### Crabs 2017 #######
 # Initialized crab tracking import. All files are filtered for speed and water
@@ -114,87 +115,6 @@ for (entry in 2:nrow(crabs201X)) {
      crabs201X[entry,"Elapsed"] = 0
    }
 }
-  
-#for (crab in unique(crabs201X$CrabNum)) {
-  #thisCrab = crabs201X[which(crabs201X$CrabNum == crab),]
-  #timeVec = thisCrab$Date[2]
-  #dateVec = as.POSIXct(paste(thisCrab$Date,thisCrab$Time),format="%m/%d/%y %H:%M:%S")
-  #dateVec2 = c(dateVec[1],dateVec[-length(dateVec)])
-  #plot(dateVec,thisCrab$Latitude) 
-  #plot(dateVec,thisCrab$Longitude)
-  #plot(dateVec,thisCrab$Distance)
-#}
-
-gaps = crabs201X[c(which(crabs201X$Elapsed > 5000),which(crabs201X$Elapsed > 5000)-1),]
-dateVecGaps = as.POSIXct(paste(gaps$Date,gaps$Time),format="%m/%d/%y %H:%M:%S")
-
-#plot(dateVecGaps,gaps$Latitude) 
-#plot(dateVec,crabs201X$Latitude)
-#plot(dateVecGaps,gaps$Longitude) 
-#plot(dateVec,crabs201X$Longitude)
-
-#Sets up crab location data frame
-gapDF = data.frame(matrix(0,ncol=11,nrow=length(unique(gaps$CrabNum))))
-colnames(gapDF) <- c("CrabNum","Island","NumEntries","TotalCocos","TotalNatives","TotalScaevola","TotalSand","AvailCocos","AvailNatives","AvailScaevola","AvailSand")
-gapDF$CrabNum <- unique(gaps$CrabNum)
-
-
-findClosestPoint <- function(x,y,df) {
-  dfclose = df[which(abs(df$lat-x) < 0.00001 & abs(df$long-y) < 0.00001),]
-  minDist = 100;
-  minRow = -1;
-  if (nrow(dfclose)==0) {
-    return(dfclose)
-  }
-  for(row in 1:nrow(dfclose)) {
-    dist = sqrt((df[row,"lat"] - x)^2 + (df[row,"long"] - y)^2)
-    if (dist < minDist) {
-      minRow = row
-      minDist = dist
-    }
-  }
-  return(dfclose[minRow,])
-}
-
-for (crab in 1:nrow(gapDF)) {
-  thisCrabTracks = gaps[gaps$CrabNum==gapDF[crab,"CrabNum"],]
-  gapDF[crab,"Island"]= thisCrabTracks[1,"Island"]
-  gapDF[crab,"NumEntries"] = nrow(thisCrabTracks)
-  gapDF[crab,"AvailCocos"] = allLocations[gapDF[crab,"Island"],"Cocos"]
-  gapDF[crab,"AvailNatives"] = allLocations[gapDF[crab,"Island"],"Natives"]
-  gapDF[crab,"AvailScaevola"] = allLocations[gapDF[crab,"Island"],"Scaevola"]
-  gapDF[crab,"AvailSand"] = allLocations[gapDF[crab,"Island"],"Sand"]
-  thisCrabIsland = islandCoordsList[[gapDF[crab,"Island"]]]
-  thisCrabTracks$isInWater = FALSE;
-  for (i in 1:nrow(thisCrabTracks)) {
-    #temp = thisCrabIsland[(thisCrabIsland[,"long"]==thisCrabTracks$Longitude[i]&thisCrabIsland[,"lat"]==thisCrabTracks$Latitude[i]),]
-    #temp = r.coordpts@data[(r.coordpts@data[,"long"]==thisCrabTracks$Longitude[i]&r.coordpts@data[,"lat"]==thisCrabTracks$Latitude[i]),]
-    temp = findClosestPoint(thisCrabTracks$Latitude[i], thisCrabTracks$Longitude[i], thisCrabIsland)
-    if (nrow(temp) != 0) {
-      for (j in 1:nrow(temp)) {
-        if (temp[j,1] == 0) {
-          gapDF[crab,"TotalCocos"] = gapDF[crab,"TotalCocos"] + 1
-        } else if (temp[j,1] == 1) {
-          gapDF[crab,"TotalNatives"] = gapDF[crab,"TotalNatives"] + 1
-        } else if (temp[j,1] == 2) {
-          gapDF[crab,"TotalScaevola"] = gapDF[crab,"TotalScaevola"] + 1
-        } else if (temp[j,1] == 5) {
-          gapDF[crab,"TotalSand"] = gapDF[crab,"TotalSand"] + 1
-        }
-      }
-    } 
-  }
-}
-
-gapDF[,c("TotalCocos","TotalNatives","TotalScaevola","TotalSand")] = (gapDF[,c("TotalCocos","TotalNatives","TotalScaevola","TotalSand")]+0.01)/rowSums(gapDF[,c("TotalCocos","TotalNatives","TotalScaevola","TotalSand")])
-
-coordinates(gaps) <- ~Longitude+Latitude
-proj4string(gaps)<-CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")
-gaps@data = gaps@data[,1:9]
-shapefile(gaps, "gapsVisualTenHours.shp")
-widesIII(gapDF[,c("TotalCocos","TotalNatives","TotalScaevola","TotalSand")],gapDF[,c("AvailCocos","AvailNatives","AvailScaevola","AvailSand")])
-
-
 
 
 abacus<-function(state_rec, times,states=NULL,labels=NULL,add=FALSE,xlim=NULL,tunit="month", format="%m/%y",col="black",
@@ -256,7 +176,7 @@ for (crab in unique(crabs201X$CrabNum)) {
 #Plotting how many crab hits per half an hour 
 #crabNumVec = crabs201X$CrabNum
 #thisDateVec = as.POSIXct(paste(crabs201X$Date, crabs201X$Time),format="%m/%d/%y %H:%M:%S")
-#wwwwpdf("7.12CrabHitsPerHour.pdf")
+#pdf("7.12CrabHitsPerHour.pdf")
 for (crab in unique(crabs201X$CrabNum)) {
   thisCrabTrax = subset(crabs201X,CrabNum==crab) 
   thisDateVec = as.POSIXct(paste(thisCrabTrax$Date,thisCrabTrax$Time),format="%m/%d/%y %H:%M:%S")
@@ -360,6 +280,7 @@ data_summary <- function(data, varname, groupnames){
   return(data_sum)
 }
 
+#pdf("Crabs201617CompiledAnalysis.pdf")
 
 meltmcpallwiframe <- melt(mcpallwiframe,id=c("CrabNum","Island","Year"))
 meltmcpallwiframe$type = revalue(substr(meltmcpallwiframe$variable,1,2),c("95" = "mcp95","50" = "mcp50"))
@@ -373,7 +294,7 @@ dfall = rbind(dfmcpall,dfkudall)
 #All crbas ever, plotting in general
 p <- ggplot(data=dfall,aes(x=variable,y=value,fill=type)) + geom_bar(stat="identity",position=position_dodge()) +
   geom_errorbar(aes(ymin=value-sd, ymax=value+sd), width=.2,
-                position=position_dodge(.9))
+                position=position_dodge(.9)) + ggtitle("Average Habitat Selection Ratio for Coconut Crabs 16-17")
 p
 
 #All crabs ever, plotting by year
@@ -384,7 +305,7 @@ dfkudallvy <- data_summary(meltkudallwiframe,varname="value",groupnames=c("type"
 dfallvy = rbind(dfmcpallvy,dfkudallvy)
 p <- ggplot(data=dfallvy,aes(x=varYear,y=value,fill=type)) + geom_bar(stat="identity",position=position_dodge()) +
   geom_errorbar(aes(ymin=value-sd, ymax=value+sd), width=.2,
-                position=position_dodge(.9))
+                position=position_dodge(.9)) +  ggtitle("Average Habitat Selection Ratio for Coconut Crabs By Year")
 p
 
 #All crabs ever, plotting by island 
@@ -396,7 +317,7 @@ dfallvi = rbind(dfmcpallvi,dfkudallvi)
 p <- ggplot(data=dfallvi,aes(x=varIsle,y=value,fill=type)) + geom_bar(stat="identity",position=position_dodge()) +
   geom_errorbar(aes(ymin=value-sd, ymax=value+sd), width=.2,
                 position=position_dodge(.9)) + theme(text = element_text(size=10),
-                                                     axis.text.x = element_text(angle=60, hjust=1))
+                                                     axis.text.x = element_text(angle=60, hjust=1)) +  ggtitle("Average Habitat Selection Ratio for Coconut Crabs By Island")
 p
 
 #Analysis of crab selection by island
@@ -422,10 +343,11 @@ relativeall$sd = as.numeric(relativeall$sd)
 p <- ggplot(data=relativeall,aes(x=varIsle,y=value,fill=type)) + geom_bar(stat="identity",position=position_dodge()) +
   geom_errorbar(aes(ymin=value-sd, ymax=value+sd), width=.2,
                 position=position_dodge(.9)) + theme(text = element_text(size=10),
-                                                     axis.text.x = element_text(angle=60, hjust=1)) 
+                                                     axis.text.x = element_text(angle=60, hjust=1)) + ggtitle("% Crab Home Range in Each Habitat Type by Island, Compared to Real Habitat Ratio")
 #+ theme_minimal()
 p
 
+#dev.off()
 
 
 
